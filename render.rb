95bfe37405ec4f44e04111d3b6d6d5a3e5a18f6b
Aearnus <tslimkemann42@gmail.com>
2019-02-08 00:42:08 -0700

    ooh

--- render.rb
+++ render.rb
@@ -1,9 +1,23 @@
 #!/usr/bin/env ruby
 
 require "erb"
+require "yaml"
+require "ostruct"
 
-Dir["**/*.erb"].each do |fullpath|
+# render top level templates
+Dir["*.erb"].each do |fullpath|
+  puts "rendering #{fullpath}"
   dirname = File.dirname fullpath
   filename = File.basename fullpath, ".erb"
   File.write(File.join(dirname, filename), ERB.new(File.read fullpath).result)
+end
+
+# render challenges using the challenges/base.html.erb template
+Dir["challenges/*.yaml"].each do |fullpath|
+  puts "rendering #{fullpath}"
+  dirname = File.dirname fullpath
+  filename = File.basename fullpath, ".yaml"
+  yaml_data = YAML::load_file fullpath
+  out_filename = File.join(dirname, filename) + ".html"
+  File.write(out_filename, ERB.new(File.read "challenges/base.html.erb").result(OpenStruct.new(yaml_data).instance_eval { binding }))
 end
